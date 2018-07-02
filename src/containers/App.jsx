d8c84bd38bbebf3f8e442c9be1e074a215dbb57e
Mateo Balcorta <misterrattail@gmail.com>
2018-07-02 10:27:06 -0700

    Updated tipe document for text.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -32,7 +32,7 @@ class App extends Component {
 
         axios({
           method: "get",
-          url: 'https://api.tipe.io/api/v1/document/5af33e3968f0a10013429fdc',
+          url: 'https://api.tipe.io/api/v1/document/5b3a5a66effc6a0013baa2aa',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': tipeAuth,
