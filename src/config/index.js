25921d78d9bed599a30a80f3ca8da4057f8c27e5
Mateo Balcorta <misterrattail@gmail.com>
2018-08-21 12:07:00 -0700

    Clean Up.

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
 const CONF = {
-    actionKitPageShortName: 'FCC-submit-comment',
-    actionKitPageId: 2421
+  actionKitPageShortName: 'FCC-submit-comment',
+  actionKitPageId: 2421,
 };
 const URLS = {
-    actionKit: 'https://act.demandprogress.org/act/',
-    count: 'https://act.demandprogress.org/progress/' + CONF.actionKitPageShortName + '?callback=onFetchSignatureCounts',
-    facebook: 'https://www.facebook.com/sharer.php?u=',
-    twitter:  'https://twitter.com/intent/tweet?text=',
-    feedack: 'https://dp-feedback-tool.herokuapp.com/api/v1/feedback?'
+  actionKit: 'https://act.demandprogress.org/act/',
+  count: `https://act.demandprogress.org/progress/${CONF.actionKitPageShortName}?callback=onFetchSignatureCounts`,
+  facebook: 'https://www.facebook.com/sharer.php?u=',
+  twitter: 'https://twitter.com/intent/tweet?text=',
+  feedack: 'https://dp-feedback-tool.herokuapp.com/api/v1/feedback?',
 };
 
 export { CONF, URLS };
