9a87db7c9648aafc0c587fa279be22ea976a2a1a
Mateo Balcorta <misterrattail@gmail.com>
2018-09-21 09:11:52 -0700

    Update variable for comment.

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import Form from './Form.jsx';
 import Markdown from 'react-markdown';
+import Form from './Form.jsx';
 
-const Main = (props) =>  {
-    return (
+const Main = props => (
     <div id="app">
         <div className="unit">
             <div className="hero" id="bftn-action-form">
                 <div>
                     <div id="signThePetition">
-                        <Form 
-                        subHeader={props.subHeader} 
-                        header={ props.header } 
-                        ftcComment = { props.ftcComment }
-                        main={props.main} 
-                        disclaimer={props.disclaimer} 
-                        formButton = {props.formButton} 
-                        modalHeader = {props.modalHeader}
-                        modalText = {props.modalText}/>
+                        <Form
+                        subHeader={ props.subHeader }
+                        header={ props.header }
+                        fccComment = { props.fccComment }
+                        main={ props.main }
+                        disclaimer={ props.disclaimer }
+                        formButton = { props.formButton }
+                        modalHeader = { props.modalHeader }
+                        modalText = { props.modalText }/>
                     </div>
                 </div>
                 <div className="unit" >
                 <div id="congress">
-                    <Markdown source= {props.congressLanguage} />
+                    <Markdown source= { props.congressLanguage } />
                 </div>
                 <hr/>
                     <div className="more_info">
                     <h4>More Information:</h4>
-                    <Markdown source= {props.main} />
+                    <Markdown source= { props.main } />
                     </div>
                 </div>
             </div>
         </div>
     </div>);
-}
 
 export default Main;
